a11a5cf2b0219db05402ccf024ec68ab5f40c70c
NatashaSh <140170565+natasha1812@users.noreply.github.com>
2025-05-19 20:49:52 +0300

    Second-upload

--- js/game.js
+++ js/game.js
@@ -22,7 +22,7 @@ var gBoard
 
 function onInit() {
     gBoard = buildBoard(gLevel.SIZE)
-    addMines(gBoard)
+    addRandomMines(gBoard)
     setMinesNegsCount(gBoard)
 
     //gBoard[1][2].isRevealed = true
@@ -31,7 +31,7 @@ function onInit() {
     //gBoard[0][1].isRevealed = true
     //gBoard[2][2].isRevealed = true
     //gBoard[3][2].isRevealed = true
-    
+
     //revealAll(gBoard)
 
     renderBoard(gBoard)
@@ -59,17 +59,31 @@ function createCell() {
     }
 }
 
-function addMines(board) {
+function addRandomMines(board) {
     board[1][2].isMine = true
     board[2][3].isMine = true
+
+    /*
+    var addedMines = 0
+
+    while (addedMines < gLevel.MINES) {
+        var i = getRandomInt(gLevel.SIZE)
+        var j = getRandomInt(gLevel.SIZE)
+
+        if (!board[i][j].isMine) {
+            board[i][j].isMine = true
+            addedMines++
+        }
+    }
+        */
 }
 
 function renderBoard(board) {
     const elTable = document.querySelector('.game-board')
-    var htmlStr = '<tbody>'
+    var strHTML = '<tbody>'
 
     for (var i = 0; i < board.length; i++) {
-        htmlStr += '<tr>'
+        strHTML += '<tr>'
         for (var j = 0; j < board[i].length; j++) {
             const cell = board[i][j]
             const cellId = `cell-${i}-${j}`
@@ -86,13 +100,14 @@ function renderBoard(board) {
                 cellContent = FLAG_IMG
             }
 
-            htmlStr += `<td id="${cellId}" onclick="onCellClicked(${i}, ${j})">${cellContent}</td>`
+            strHTML += `<td id="${cellId}" onclick="onCellClicked(this, ${i}, ${j})" 
+            oncontextmenu="onCellMarked(this, ${i}, ${j})"> ${cellContent}</td>`
         }
-        htmlStr += '</tr>'
+        strHTML += '</tr>'
     }
 
-    htmlStr += '</tbody>'
-    elTable.innerHTML = htmlStr
+    strHTML += '</tbody>'
+    elTable.innerHTML = strHTML
 }
 
 function setMinesNegsCount(board) {
@@ -119,26 +134,87 @@ function countNegs(cellI, cellJ, board) {
 }
 
 function onCellClicked(elCell, i, j) {
+    var cell = gBoard[i][j]
 
+    if (cell.isRevealed || cell.isMarked) return
+
+    cell.isRevealed = true
+
+    if (cell.isMine) {
+        elCell.innerHTML = MINE_IMG
+    } else if (cell.minesAroundCount > 0) {
+        elCell.innerText = cell.minesAroundCount
+    } else {
+        elCell.innerText = ''
+    }
+    checkGameOver()
 }
 
 function onCellMarked(elCell, i, j) {
+    var cell = gBoard[i][j]
 
+    if (cell.isRevealed) return
+
+    cell.isMarked = !cell.isMarked
+
+    if (cell.isMarked) {
+        elCell.innerHTML = FLAG_IMG
+    } else if (!cell.isMarked) {
+        elCell.innerText = ''
+    }
+    checkGameOver()
 }
 
 function checkGameOver() {
     //The game ends when all mines are marked, and all the other cells are revealed
+    //Mine is clicked or all mines are marked and other cells are revealed
+
+    var totalCells = gLevel.SIZE ** 2
+    var cellsRevealed = 0
+    var minesMarkedCorrectly = 0
+    var mineClicked = false
+
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[i].length; j++) {
+            var cell = gBoard[i][j]
+
+            if (cell.isRevealed) {
+                cellsRevealed++
+                if (cell.isMine) mineClicked = true
+            }
+
+            if (cell.isMarked && cell.isMine) {
+                minesMarkedCorrectly++
+            }
+        }
+    }
+
+    if (mineClicked) {
+        console.log('You lost.')
+        gGame.isOn = false
+        return
+    }
+
+    if (cellsRevealed === (totalCells - gLevel.MINES) &&
+        minesMarkedCorrectly === gLevel.MINES) {
+        console.log('You won!')
+        gGame.isOn = false
+    }
 }
 
-function expandReveal(board, elCell, i, j) {
+function expandReveal(i, j, elCell, board) {
     //When the user clicks a cell with no mines around, 
     //reveal not only that cell, but also its neighbors
 }
 
 function revealAll(board) {
     for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-        board[i][j].isRevealed = true
-      }
+        for (let j = 0; j < board[i].length; j++) {
+            board[i][j].isRevealed = true
+        }
     }
-  }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
